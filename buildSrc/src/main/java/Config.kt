8f0a7e34f93cb1d7c0658e587508f22be93288ff
mohsen ataei <mohsenataeiasad@gmail.com>
2020-09-08 00:14:22 +0430

    some 3rd party and jetpack libs added to buildSrc for further use.

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -2,6 +2,26 @@ object Config {
     object Versions {
         const val kotlin = "1.3.72"
         const val gradle = "4.0.0"
+
+        const val arrow = "0.10.4"
+        const val ktlint = "0.37.2"
+        const val coroutines = "1.3.3"
+        const val gradleVersions = "0.27.0"
+        const val arch = "2.2.0"
+        const val archTest = "2.1.0"
+        const val material = "1.0.0"
+        const val constraintLayout = "1.1.3"
+        const val room = "2.2.5"
+        const val dagger = "2.25.4"
+        const val okHttp = "3.8.1"
+        const val gson = "2.8.6"
+        const val retrofit = "2.7.1"
+        const val stetho = "1.5.1"
+        const val glide = "4.11.0"
+        const val junit = "4.12"
+        const val testRunner = "1.2.0"
+        const val espresso = "3.2.0"
+        const val mockitoKotlin = "1.6.0"
     }
 
     object BuildPlugins {
@@ -35,14 +55,48 @@ object Config {
         const val androidXConstraintLayout = "androidx.constraintlayout:constraintlayout:2.0.1"
     }
 
+    object JetpackLibs {
+        const val lifecycleExtensions = "androidx.lifecycle:lifecycle-extensions:${Versions.arch}"
+        const val lifecycleKtx = "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.arch}"
+        const val viewModelKtx = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.arch}"
+        const val livedataKtx = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.arch}"
+        const val room = "androidx.room:room-runtime:${Versions.room}"
+        const val roomKtx = "androidx.room:room-ktx:${Versions.room}"
+        const val roomCompiler = "androidx.room:room-compiler:${Versions.room}"
+        const val material = "com.google.android.material:material:${Versions.material}"
+        const val annotations = "androidx.annotation:annotation:${Versions.material}"
+        const val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+    }
+
+    object CommonLibs {
+        const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlin}"
+        const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}"
+        const val coroutinesAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutines}"
+        const val arrowCore = "io.arrow-kt:arrow-core:${Versions.arrow}"
+        const val arrowSyntax = "io.arrow-kt:arrow-syntax:${Versions.arrow}"
+        const val arrowMeta = "io.arrow-kt:arrow-meta:${Versions.arrow}"
+        const val dagger = "com.google.dagger:dagger:${Versions.dagger}"
+        const val daggerCompiler = "com.google.dagger:dagger-compiler:${Versions.dagger}"
+        const val daggerAndroid = "com.google.dagger:dagger-android-support:${Versions.dagger}"
+        const val daggerAndroidCompiler = "com.google.dagger:dagger-android-processor:${Versions.dagger}"
+        const val gson = "com.google.code.gson:gson:${Versions.gson}"
+        const val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+        const val retrofitGson = "com.squareup.retrofit2:converter-gson:${Versions.retrofit}"
+        const val okHttpInterceptor = "com.squareup.okhttp3:logging-interceptor:${Versions.okHttp}"
+        const val stetho = "com.facebook.stetho:stetho:${Versions.stetho}"
+        const val stetho_OkHttp = "com.facebook.stetho:stetho-okhttp3:${Versions.stetho}"
+        const val glide = "com.github.bumptech.glide:glide:${Versions.glide}"
+        const val glideCompiler = "com.github.bumptech.glide:compiler:${Versions.glide}"
+    }
     object TestLibs {
+        const val junit = "junit:junit:${Versions.junit}"
+        const val testRunner = "androidx.test:runner:${Versions.testRunner}"
+        const val esperesso = "androidx.test.espresso:espresso-core:${Versions.espresso}"
+        const val mockitoKotlin = "com.nhaarman:mockito-kotlin-kt1.1:${Versions.mockitoKotlin}"
+        const val archTesting = "androidx.arch.core:core-testing:${Versions.archTest}"
         const val jUnit = "junit:junit:4.12"
         const val extJUnit = "androidx.test.ext:junit:1.1.2"
         const val espresso = "androidx.test.espresso:espresso-core:3.3.0"
-    }
-
-    object Modules {
-
     }
 
 }
