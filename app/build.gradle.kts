541990f9ace24a02497a2f0c51e94a8fcf00b6e5
mohsen ataei <mohsenataeiasad@gmail.com>
2020-10-03 01:20:57 +0330

    some libraries added.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,10 +19,17 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    dataBinding {
+        isEnabled = true
+    }
+
     buildTypes {
         getByName(Config.BuildTypes.RELEASE) {
-            isMinifyEnabled =  false
-            proguardFiles (getDefaultProguardFile ("proguard-android-optimize.txt"), "proguard-rules.pro")
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 }
@@ -37,4 +44,49 @@ dependencies {
     testImplementation(Config.TestLibs.jUnit)
     testImplementation(Config.TestLibs.extJUnit)
     testImplementation(Config.TestLibs.espresso)
+
+    implementation(Config.CommonLibs.coroutines)
+    implementation(Config.CommonLibs.coroutinesAndroid)
+    implementation(Config.CommonLibs.arrowCore)
+    implementation(Config.CommonLibs.arrowSyntax)
+    kapt(Config.CommonLibs.arrowSyntax)
+
+    // jetPack
+    implementation(Config.JetpackLibs.lifecycleExtensions)
+    implementation(Config.JetpackLibs.viewModelKtx)
+    implementation(Config.JetpackLibs.livedataKtx)
+    implementation(Config.JetpackLibs.room)
+    implementation(Config.JetpackLibs.material)
+    implementation(Config.JetpackLibs.constraintLayout)
+
+    //TODO ("add hilt dependency injection later.")
+    //dagger
+    implementation(Config.CommonLibs.dagger)
+    implementation(Config.CommonLibs.daggerAndroid)
+    kapt(Config.CommonLibs.daggerCompiler)
+    kapt(Config.CommonLibs.daggerAndroidCompiler)
+
+
+    //retrofit and okhttp
+    implementation(Config.CommonLibs.retrofit)
+    implementation(Config.CommonLibs.retrofitGson)
+    implementation(Config.CommonLibs.okHttpInterceptor)
+    implementation(Config.CommonLibs.stetho)
+    implementation(Config.CommonLibs.stetho_OkHttp)
+
+    //Glide
+
+    implementation(Config.CommonLibs.glide)
+    kapt(Config.CommonLibs.glideCompiler)
+
+    /*
+    *  implementation Libs.Jetpack.lifecycleExtensions
+    implementation Libs.Jetpack.viewModelKtx
+    implementation Libs.Jetpack.livedataKtx
+    implementation Libs.Jetpack.room
+    implementation Libs.Jetpack.material
+    implementation Libs.Jetpack.constraintLayout
+    *
+    * */
+
 }
